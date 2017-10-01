2532d360482a79be94d51f5ed1be813bc4a1aef7
ciro <ciromanzar@bitbucket.org>
2017-10-01 20:04:38 -0300

    .

--- tp2/solver.c
+++ tp2/solver.c
@@ -101,7 +101,7 @@ void solver_advect ( fluid_solver* solver, uint32_t b, float * d, float * d0, fl
 }
 
 /* SIMD IMPLEMENTATION */
-/*
+
 void solver_lin_solve ( fluid_solver* solver, uint32_t b, float * x, float * x0, float a, float c ){
 	uint32_t i, j, k;
 	for ( k=0 ; k<20 ; k++ ) {
@@ -110,7 +110,7 @@ void solver_lin_solve ( fluid_solver* solver, uint32_t b, float * x, float * x0,
 		END_FOR
 		solver_set_bnd ( solver, b, x );
 	}
-}*/
+}
 
 void solver_set_bnd ( fluid_solver* solver, uint32_t b, float * x ){
 	uint32_t i;
