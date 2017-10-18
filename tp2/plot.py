d399589d510f35adfe08007c33190998530a48e9
ciro <ciromanzar@bitbucket.org>
2017-10-18 20:13:06 -0300

    script tiempos

---
+++ tp2/plot.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set_style("darkgrid")
+import csv
+from scipy import stats
+
+
+tamanios = 25
+salto_medicion = 4
+distintos = 10
+iguales = 100 
+
+optimizacion = "3"
+
+def procesarTiempos(direccion):
+	with open(direccion, 'r') as my_file:
+	    reader = csv.reader(my_file, delimiter='\n')
+	    my_list = list(reader)
+
+	    tiempos = []
+	    for numero in my_list:
+	    	tiempos.append(int(numero[0]) )
+
+	    mediciones = []
+	    for tam in range(0,tamanios):
+	    	list_distintos = []
+	    	for dist in range(0, distintos):
+	    		list_iguales = []
+	    		for igu in range(0,iguales):
+	    			list_iguales.append(tiempos[tam*distintos*iguales + dist*iguales + igu])
+	    		list_distintos.append(min(list_iguales))
+	     	mediciones.append(np.mean(list_distintos))
+
+	return mediciones
+
+def guardarTiempos(direccion, ejex, ejey):
+	with open(direccion, 'w') as my_file:
+		for i in range(len(ejex)):
+			my_file.write(str(ejex[i]) + ' ' + str(ejey[i]) + '\n')
+
+mediciones_c = procesarTiempos("output/tiempos_solver_lin_solve_c_o"+optimizacion+".out")
+mediciones_1px = procesarTiempos("output/tiempos_solver_lin_solve_asm_1px_o"+optimizacion+".out")
+mediciones_2px = procesarTiempos("output/tiempos_solver_lin_solve_asm_2px_o"+optimizacion+".out")
+mediciones_opt = procesarTiempos("output/tiempos_solver_lin_solve_asm_opt_o"+optimizacion+".out")
+
+'''
+print(mediciones_c)
+print(mediciones_1px)
+print(mediciones_2px)
+print(mediciones_opt)
+'''
+
+eje_x = [(i+1)*salto_medicion for i in range(0, tamanios)]
+
+guardarTiempos("output/tiempos_procesados_lin_c_o"+optimizacion+".out", eje_x, mediciones_c)
+guardarTiempos("output/tiempos_procesados_lin_asm_1px_o"+optimizacion+".out", eje_x, mediciones_1px)
+guardarTiempos("output/tiempos_procesados_lin_asm_2px_o"+optimizacion+".out", eje_x, mediciones_2px)
+guardarTiempos("output/tiempos_procesados_lin_asm_opt_o"+optimizacion+".out", eje_x, mediciones_opt)
+
+c = pd.read_csv("output/tiempos_procesados_lin_c_o"+optimizacion+".out", sep=' ')
+simd1 = pd.read_csv("output/tiempos_procesados_lin_asm_1px_o"+optimizacion+".out", sep=' ')
+simd2 = pd.read_csv("output/tiempos_procesados_lin_asm_2px_o"+optimizacion+".out", sep=' ')
+simdo = pd.read_csv("output/tiempos_procesados_lin_asm_opt_o"+optimizacion+".out", sep=' ')
+
+g = sns.lmplot(x="Dimension", y="Tiempo", data=c)
+#.set_ylabel('Tiempo (Nanosegundos)')
+'''
+sns.lmplot(time=simd1['x'], data=simd1['tiempo'], interpolate=True, color="yellow").set_xlabel('Dimension')
+sns.lmplot(time=simd2['x'], data=simd2['tiempo'], interpolate=True, color="blue")
+sns.lmplot(time=simdo['x'], data=simdo['tiempo'], interpolate=True, color="red")
+'''
+# sns.show()
+
+
--- tp2/prueba_tiempos.c
+++ tp2/prueba_tiempos.c
@@ -10,11 +10,11 @@
 
 fluid_solver* solver;
 
-char *archivo_out_c  =  "tiempos_solver_lin_solve_c_o3out";
+char *archivo_out_c  =  "tiempos_solver_lin_solve_c_o3.out";
 char *archivo_out_asm_1px  =  "tiempos_solver_lin_solve_asm_1px_o3.out";
 char *archivo_out_asm_2px  =  "tiempos_solver_lin_solve_asm_2px_o3.out";
 char *archivo_out_asm_opt  =  "tiempos_solver_lin_solve_asm_opt_o3.out";
-int ITERACIONESDISTINTAS = 100;
+int ITERACIONESDISTINTAS = 10;
 int ITERACIONESIGUALES = 100;
 
 
@@ -42,7 +42,7 @@ int main(){
 	remove(archivo_out_asm_2px);
 	remove(archivo_out_asm_opt);
 	
-	FILE *fc = fopen(archivo_out_c,"ab+");
+	FILE *fc = fopen(archivo_out_c,"ab+"); 
 	FILE *fa1 = fopen(archivo_out_asm_1px,"ab+");
 	FILE *fa2 = fopen(archivo_out_asm_2px,"ab+");
 	FILE *fao = fopen(archivo_out_asm_opt,"ab+");
@@ -51,16 +51,7 @@ int main(){
 		printf("error");	
 	}
 
-	double promedio_c = 0;
-	double promedio_asm_1 = 0;
-	double promedio_asm_2 = 0;
-	double promedio_asm_o = 0;
-
-	for(int i = 400; i <= 400; i = i + 8){
-		double total_iteracion_c = 0;
-		double total_iteracion_asm_1px = 0;
-		double total_iteracion_asm_2px = 0;
-		double total_iteracion_asm_opt = 0;
+	for(int i = 4; i <= 100; i = i + 4){
 
 		for(int iteracion = 0; iteracion < ITERACIONESDISTINTAS; iteracion++){
 			int inner_size = i;
@@ -83,70 +74,35 @@ int main(){
 				clock_t begin_c = clock();
 				solver_lin_solve(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_c = clock();
-				total_medicion_c += (double)(end_c - begin_c);
-
+				total_medicion_c += (end_c - begin_c);
 
 				clock_t begin_asm_1 = clock();
 				solver_lin_solve_1pixel_por_lectura(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_asm_1 = clock();
-				total_medicion_asm_1px += (double)(end_asm_1 - begin_asm_1);
+				total_medicion_asm_1px += (end_asm_1 - begin_asm_1);
 
 				clock_t begin_asm_2 = clock();
 				solver_lin_solve_2pixel_por_lectura(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_asm_2 = clock();
-				total_medicion_asm_2px += (double)(end_asm_2 - begin_asm_2);
+				total_medicion_asm_2px += (end_asm_2 - begin_asm_2);
 
 				clock_t begin_asm_opt = clock();
 				solver_lin_solve_2pixel_optimo(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_asm_opt = clock();
-				total_medicion_asm_opt += (double)(end_asm_opt - begin_asm_opt);
+				total_medicion_asm_opt += (end_asm_opt - begin_asm_opt);
+				
+				fprintf(fc,"%i\n", total_medicion_c);
+
+				fprintf(fa1,"%i\n", total_medicion_asm_1px);
+
+				fprintf(fa2,"%i\n", total_medicion_asm_2px);
+
+				fprintf(fao,"%i\n", total_medicion_asm_opt);
 			}
-			total_iteracion_c += total_medicion_c/(float)ITERACIONESIGUALES;
-			total_iteracion_asm_1px += total_medicion_asm_1px/(float)ITERACIONESIGUALES;
-			total_iteracion_asm_2px += total_medicion_asm_2px/(float)ITERACIONESIGUALES;
-			total_iteracion_asm_opt += total_medicion_asm_opt/(float)ITERACIONESIGUALES;
 		}
-
-
-		total_iteracion_c = total_iteracion_c / (float)ITERACIONESDISTINTAS;
-		total_iteracion_asm_1px = total_iteracion_asm_1px / (float)ITERACIONESDISTINTAS;
-		total_iteracion_asm_2px = total_iteracion_asm_2px / (float)ITERACIONESDISTINTAS;
-		total_iteracion_asm_opt = total_iteracion_asm_opt / (float)ITERACIONESDISTINTAS;
-
-		promedio_c += total_iteracion_c;
-		promedio_asm_1 += total_iteracion_asm_1px;
-		promedio_asm_2 += total_iteracion_asm_2px;
-		promedio_asm_o += total_iteracion_asm_opt;
-
-		printf("iteracion c %d %f\n", i, total_iteracion_c);
-		fprintf(fc,"%d %f\n", i, total_iteracion_c);
-
-		printf("iteracion asm 1px %d %f\n", i, total_iteracion_asm_1px);
-		fprintf(fa1,"%d %f\n", i, total_iteracion_asm_1px);
-
-		printf("iteracion asm 2px %d %f\n", i, total_iteracion_asm_2px);
-		fprintf(fa2,"%d %f\n", i, total_iteracion_asm_2px);
-
-		printf("iteracion asm opt %d %f\n \n", i, total_iteracion_asm_opt);
-		fprintf(fao,"%d %f\n", i, total_iteracion_asm_opt);
-		
+		printf("iteracion %i\n", i);
 	}
-	
-	promedio_c = promedio_c / 1.0f ;
-	printf("promedio c %f\n", promedio_c);
-	fprintf(fc," %f\n", promedio_c);
 
-	promedio_asm_1 = promedio_asm_1 / 1.0f ;
-	printf("promedio asm 1px %f\n", promedio_asm_1);
-	fprintf(fa1," %f\n", promedio_asm_1);
-
-	promedio_asm_2 = promedio_asm_2 / 1.0f ;
-	printf("promedio asm 2px %f\n", promedio_asm_2);
-	fprintf(fa2," %f\n", promedio_asm_2);
-
-	promedio_asm_o = promedio_asm_o / 1.0f ;
-	printf("promedio asm opt %f\n", promedio_asm_o);
-	fprintf(fao," %f\n", promedio_asm_o);
 
 	fclose(fc);
 	fclose(fa1);	
