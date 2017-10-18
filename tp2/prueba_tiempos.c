757e6138dc37845e68386f6de56606691f11bae3
ciro <ciromanzar@bitbucket.org>
2017-10-17 23:51:06 -0300

    tiempos grandes

--- tp2/prueba_tiempos.c
+++ tp2/prueba_tiempos.c
@@ -10,11 +10,11 @@
 
 fluid_solver* solver;
 
-char *archivo_out_c  =  "tiempos_solver_lin_solve_c_o0.out";
-/*char *archivo_out_asm_1px  =  "tiempos_solver_lin_solve_asm_1px_o0.out";
-char *archivo_out_asm_2px  =  "tiempos_solver_lin_solve_asm_2px_o0.out";
-char *archivo_out_asm_opt  =  "tiempos_solver_lin_solve_asm_opt_o0.out";*/
-int ITERACIONESDISTINTAS = 10;
+char *archivo_out_c  =  "tiempos_solver_lin_solve_c_o3out";
+char *archivo_out_asm_1px  =  "tiempos_solver_lin_solve_asm_1px_o3.out";
+char *archivo_out_asm_2px  =  "tiempos_solver_lin_solve_asm_2px_o3.out";
+char *archivo_out_asm_opt  =  "tiempos_solver_lin_solve_asm_opt_o3.out";
+int ITERACIONESDISTINTAS = 100;
 int ITERACIONESIGUALES = 100;
 
 
@@ -38,13 +38,15 @@ void printMatriz(float * x, int N){
 
 int main(){
 	remove(archivo_out_c);
-	/*remove(archivo_out_asm_1px);
+	remove(archivo_out_asm_1px);
 	remove(archivo_out_asm_2px);
-	remove(archivo_out_asm_opt);*/
+	remove(archivo_out_asm_opt);
+	
 	FILE *fc = fopen(archivo_out_c,"ab+");
-	/*FILE *fa1 = fopen(archivo_out_asm_1px,"ab+");
+	FILE *fa1 = fopen(archivo_out_asm_1px,"ab+");
 	FILE *fa2 = fopen(archivo_out_asm_2px,"ab+");
-	FILE *fao = fopen(archivo_out_asm_opt,"ab+");*/
+	FILE *fao = fopen(archivo_out_asm_opt,"ab+");
+
 	if(fc == NULL){
 		printf("error");	
 	}
@@ -54,11 +56,12 @@ int main(){
 	double promedio_asm_2 = 0;
 	double promedio_asm_o = 0;
 
-	for(int i = 8; i <= 400; i = i + 8){
+	for(int i = 400; i <= 400; i = i + 8){
 		double total_iteracion_c = 0;
-		/*double total_iteracion_asm_1px = 0;
+		double total_iteracion_asm_1px = 0;
 		double total_iteracion_asm_2px = 0;
-		double total_iteracion_asm_opt = 0;*/
+		double total_iteracion_asm_opt = 0;
+
 		for(int iteracion = 0; iteracion < ITERACIONESDISTINTAS; iteracion++){
 			int inner_size = i;
 			int size = (inner_size + 2)*(inner_size + 2);
@@ -83,7 +86,7 @@ int main(){
 				total_medicion_c += (double)(end_c - begin_c);
 
 
-				/*clock_t begin_asm_1 = clock();
+				clock_t begin_asm_1 = clock();
 				solver_lin_solve_1pixel_por_lectura(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_asm_1 = clock();
 				total_medicion_asm_1px += (double)(end_asm_1 - begin_asm_1);
@@ -96,60 +99,59 @@ int main(){
 				clock_t begin_asm_opt = clock();
 				solver_lin_solve_2pixel_optimo(solver, 1, x, x0, 30.0, 15.0);
 				clock_t end_asm_opt = clock();
-				total_medicion_asm_opt += (double)(end_asm_opt - begin_asm_opt);*/
+				total_medicion_asm_opt += (double)(end_asm_opt - begin_asm_opt);
 			}
 			total_iteracion_c += total_medicion_c/(float)ITERACIONESIGUALES;
-			/*total_iteracion_asm_1px += total_medicion_asm_1px/(float)ITERACIONESIGUALES;		
-			total_iteracion_asm_2px += total_medicion_asm_2px/(float)ITERACIONESIGUALES;	
-			total_iteracion_asm_opt += total_medicion_asm_opt/(float)ITERACIONESIGUALES;			*/
+			total_iteracion_asm_1px += total_medicion_asm_1px/(float)ITERACIONESIGUALES;
+			total_iteracion_asm_2px += total_medicion_asm_2px/(float)ITERACIONESIGUALES;
+			total_iteracion_asm_opt += total_medicion_asm_opt/(float)ITERACIONESIGUALES;
 		}
 
 
 		total_iteracion_c = total_iteracion_c / (float)ITERACIONESDISTINTAS;
-		/*total_iteracion_asm_1px = total_iteracion_asm_1px / (float)ITERACIONESDISTINTAS;
+		total_iteracion_asm_1px = total_iteracion_asm_1px / (float)ITERACIONESDISTINTAS;
 		total_iteracion_asm_2px = total_iteracion_asm_2px / (float)ITERACIONESDISTINTAS;
-		total_iteracion_asm_opt = total_iteracion_asm_opt / (float)ITERACIONESDISTINTAS;*/
+		total_iteracion_asm_opt = total_iteracion_asm_opt / (float)ITERACIONESDISTINTAS;
 
 		promedio_c += total_iteracion_c;
-		/*promedio_asm_1 += total_iteracion_asm_1px;
+		promedio_asm_1 += total_iteracion_asm_1px;
 		promedio_asm_2 += total_iteracion_asm_2px;
-		promedio_asm_o += total_iteracion_asm_opt;*/
-
+		promedio_asm_o += total_iteracion_asm_opt;
 
 		printf("iteracion c %d %f\n", i, total_iteracion_c);
 		fprintf(fc,"%d %f\n", i, total_iteracion_c);
 
-		/*printf("iteracion asm 1px %d %f\n", i, total_iteracion_asm_1px);
+		printf("iteracion asm 1px %d %f\n", i, total_iteracion_asm_1px);
 		fprintf(fa1,"%d %f\n", i, total_iteracion_asm_1px);
 
 		printf("iteracion asm 2px %d %f\n", i, total_iteracion_asm_2px);
 		fprintf(fa2,"%d %f\n", i, total_iteracion_asm_2px);
 
 		printf("iteracion asm opt %d %f\n \n", i, total_iteracion_asm_opt);
-		fprintf(fao,"%d %f\n", i, total_iteracion_asm_opt);*/
+		fprintf(fao,"%d %f\n", i, total_iteracion_asm_opt);
 		
 	}
 	
-	promedio_c = promedio_c / 25.0f ;
+	promedio_c = promedio_c / 1.0f ;
 	printf("promedio c %f\n", promedio_c);
 	fprintf(fc," %f\n", promedio_c);
 
-/*	promedio_asm_1 = promedio_asm_1 / 100.0f ;
-	printf("promedio c %f\n", promedio_asm_1);
-	fprintf(fc," %f\n", promedio_asm_1);
+	promedio_asm_1 = promedio_asm_1 / 1.0f ;
+	printf("promedio asm 1px %f\n", promedio_asm_1);
+	fprintf(fa1," %f\n", promedio_asm_1);
 
-	promedio_asm_2 = promedio_asm_2 / 100.0f ;
-	printf("promedio c %f\n", promedio_asm_2);
-	fprintf(fc," %f\n", promedio_asm_2);
+	promedio_asm_2 = promedio_asm_2 / 1.0f ;
+	printf("promedio asm 2px %f\n", promedio_asm_2);
+	fprintf(fa2," %f\n", promedio_asm_2);
 
-	promedio_asm_o = promedio_asm_o / 100.0f ;
-	printf("promedio c %f\n", promedio_asm_o);
-	fprintf(fc," %f\n", promedio_asm_o);*/
+	promedio_asm_o = promedio_asm_o / 1.0f ;
+	printf("promedio asm opt %f\n", promedio_asm_o);
+	fprintf(fao," %f\n", promedio_asm_o);
 
 	fclose(fc);
-	/*fclose(fa1);	
+	fclose(fa1);	
 	fclose(fa2);	
-	fclose(fao);*/	
+	fclose(fao);	
 
 	return 0;
 
